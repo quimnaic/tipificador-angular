4e27c9c027ef421247b6ef91d3c632135285acca
Edwin Quimbay <34946874+quimnaic@users.noreply.github.com>
2025-05-23 10:44:02 -0500

    Add: Power Bi

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,14 @@ import { AuthGuard } from './auth/guards/auth.guard';
 import { UsersComponent } from './components/users/users.component';
 import { SmsComponent } from './components/sms/sms.component';
 import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
+import { DashComponent } from './dash/dash.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
-  { path: 'users', component: UsersComponent, canActivate: [AuthGuard]},
+  { path: 'users', component: UsersComponent, canActivate: [AuthGuard], data: { roles: ['admin'] } },
   { path: 'sms', component: SmsComponent, canActivate: [AuthGuard], data: { roles: ['admin'] } },
+  { path: 'powerbi', component: DashComponent, canActivate: [AuthGuard], data: { roles: ['admin']} },
   { path: 'unauthorized', component: UnauthorizedComponent },
   { path: '**', redirectTo: '/login', pathMatch: 'full' },
 ];
